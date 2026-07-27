23cd852a33eb12803cfe27a9e5d585d3fa27969e
Gabrielle Rosa <rosagabrielle385@gmail.com>
2026-07-27 16:29:57 -0300

    Add temporary /debug/ip endpoint to diagnose rate limit key on Vercel

--- src/server.js
+++ src/server.js
@@ -14,6 +14,15 @@ app.use(helmet());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+app.get("/debug/ip", (req, res) => {
+  res.json({
+    reqIp: req.ip,
+    xRealIp: req.headers["x-real-ip"],
+    xForwardedFor: req.headers["x-forwarded-for"],
+    vercelForwardedFor: req.headers["x-vercel-forwarded-for"],
+  });
+});
+
 app.use("/auth", authRoutes);
 app.use("/gastos", gastosRoutes);
 
